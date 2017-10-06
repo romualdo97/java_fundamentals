0db2710032e5932231c3d2c78034a61afcd74249
romualdo97 <romualdorojo97@gmail.com>
2017-10-06 00:58:02 -0500

    Conditionals with ternary operator started

---
+++ 03 condicionales/CondicionalesConTernario.java
@@ -0,0 +1,13 @@
+public class CondicionalesConTernario {
+
+	public static void main(String[] args) {
+		
+		int i; //Declarión de la variable
+		i = 4; //Inicialización de la variable
+		String result; // declaracion de la variable
+		result = i > 0 ? "es verdadera" : "no es verdadera"; // usando operador ternario para evaluar condicion
+		System.out.print("La condición " + result);
+
+	}
+
+}
